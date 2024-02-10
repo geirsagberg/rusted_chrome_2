539e6819be13790551988b21677beee548086822
Geir Sagberg <geir.sagberg@gmail.com>
2024-02-10 23:26:54 +0100

    Camera working

--- src/camera.rs
+++ src/camera.rs
@@ -24,25 +24,29 @@ impl Plugin for CameraPlugin {
     }
 }
 fn setup_camera(mut commands: Commands, game_world: Res<GameWorld>) {
-    // camera.transform.translation.x = game_world.width / 2.;
-    // camera.transform.translation.y = game_world.height / 2.;
-
     let mut camera2d_bundle = Camera2dBundle::default();
-    camera2d_bundle.transform.translation.x = game_world.width / 2.;
-    camera2d_bundle.transform.translation.y = game_world.height / 2.;
+    camera2d_bundle.transform.translation.x = game_world.width;
+    camera2d_bundle.transform.translation.y = game_world.height;
     commands.spawn((camera2d_bundle, PixelZoom::Fixed(2), PixelViewport));
 }
 
 fn follow_targets(
     query: Query<&Transform, With<CameraTarget>>,
-    mut camera_query: Query<
-        (&mut Transform, &PixelViewport),
-        (With<Camera>, Without<CameraTarget>),
-    >,
+    mut camera_query: Query<(&mut Transform, &Camera, &PixelZoom), (Without<CameraTarget>)>,
     time: Res<Time>,
     game_world: Res<GameWorld>,
 ) {
-    let (mut camera, pixel_viewport) = camera_query.single_mut();
+    let (mut camera_transform, camera, pixel_zoom) = camera_query.single_mut();
+
+    let viewport = camera.logical_viewport_rect().unwrap();
+    let (pixel_width, pixel_height) = match (pixel_zoom) {
+        PixelZoom::Fixed(zoom) => {
+            let pixel_width = viewport.width() as f32 / *zoom as f32;
+            let pixel_height = viewport.height() as f32 / *zoom as f32;
+            (pixel_width, pixel_height)
+        }
+        _ => (0., 0.),
+    };
 
     // find max and min x and y of targets
     let mut min_x = f32::MAX;
@@ -74,23 +78,19 @@ fn follow_targets(
 
     let follow_speed = 1.5;
 
-    if (camera.translation.x - center_x).abs() > slack {
-        let delta = (center_x - camera.translation.x) * time.delta_seconds() * follow_speed;
-        let new_x = camera.translation.x + delta;
-        // let new_x = new_x.clamp(
-        //     -pixel_viewport.left,
-        //     game_world.width - pixel_viewport.right,
-        // );
-        camera.translation.x = new_x;
+    if (camera_transform.translation.x - center_x).abs() > slack {
+        let delta =
+            (center_x - camera_transform.translation.x) * time.delta_seconds() * follow_speed;
+        let new_x = camera_transform.translation.x + delta;
+        let new_x = new_x.clamp(pixel_width / 2., game_world.width - pixel_width / 2.);
+        camera_transform.translation.x = new_x;
     }
 
-    if (camera.translation.y - center_y).abs() > slack {
-        let delta = (center_y - camera.translation.y) * time.delta_seconds() * follow_speed;
-        let new_y = camera.translation.y + delta;
-        // let new_y = new_y.clamp(
-        //     -pixel_viewport.bottom,
-        //     game_world.height - pixel_viewport.top,
-        // );
-        camera.translation.y = new_y;
+    if (camera_transform.translation.y - center_y).abs() > slack {
+        let delta =
+            (center_y - camera_transform.translation.y) * time.delta_seconds() * follow_speed;
+        let new_y = camera_transform.translation.y + delta;
+        let new_y = new_y.clamp(pixel_height / 2., game_world.height - pixel_height / 2.);
+        camera_transform.translation.y = new_y;
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(unused_parens)]
 use bevy::app::App;
 use bevy::math::vec2;
 use bevy::prelude::*;
